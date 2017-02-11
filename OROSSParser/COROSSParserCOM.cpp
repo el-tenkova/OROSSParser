6b2fe842b0b4abfbf659d6bd70bf3ac87c0b7fed
el-tenkova <el-tenkova@yandex.ru>
2017-02-11 22:22:19 +0300

    added

---
+++ OROSSParser/COROSSParserCOM.cpp
@@ -0,0 +1,112 @@
+#include "stdafx.h"
+
+#include "OROSSParser_i.h"
+#include "COROSSParserCOM.h"
+
+STDMETHODIMP COROSSParserCOM::Init( modeName Mode, long* hRes )
+{
+    *hRes = pureParser.Init((COROSSParser::modeName)Mode);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::Terminate( long* hRes )
+{
+    *hRes = pureParser.Terminate();
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::LoadArticles(BSTR Dic, /*[out, retval]*/ long *hRes)
+{
+    std::wstring tmp(Dic);
+    std::string path(tmp.begin(), tmp.end());
+    *hRes = pureParser.LoadArticles(path);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::LoadWords(BSTR Dic, /*[out, retval]*/ long *hRes)
+{
+    std::wstring tmp(Dic);
+    std::string path(tmp.begin(), tmp.end());
+    *hRes = pureParser.LoadWords(path);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddPart( BSTR Name, /*[out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddPart(Name);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddTile( BSTR Name, /* [out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddTile(Name);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AppendTile( BSTR Str, /* [out, retval]*/ long *hRes )
+{
+/*    tile ct = {curTile->second.id, curPart->second.id};
+    std::wstring name(curTile->first);
+    name.append(Str);
+    curPart->second.tiles.erase(curTile);
+    curPart->second.tiles.insert(std::pair<std::wstring, tile>(name, ct));
+    curTile = curPart->second.tiles.find(name);*/
+    *hRes = S_OK;
+    return S_OK;
+}
+
+STDMETHODIMP COROSSParserCOM::AddPara( long Num, BSTR Name, /* [out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddPara(Num, Name);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddExamplesToPara( BSTR Examples, /*[out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddExamplesToPara(Examples);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddRuleTitle(BSTR Num, BSTR Title, /* [out, retval]*/ long *hRes)
+{
+    *hRes = pureParser.AddRuleTitle(Num, Title);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddRule(BSTR Num, BSTR Name, /* [out, retval]*/ long *hRes)
+{
+    *hRes = pureParser.AddRule(Num, Name);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddInfoToRule( BSTR Info, /*[out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddInfoToRule(Info);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddFootNote( long ID, BSTR Text, /*[out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddFootNote(ID, Text);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::AddOrthogr( BSTR Orthogr, BSTR Formula, BSTR Example, BSTR Rest, long IsActive, long IsPrefix, /*[out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddOrthogr(Orthogr, Formula, Example, Rest, IsActive, IsPrefix);
+    return *hRes;
+}
+
+
+STDMETHODIMP COROSSParserCOM::AddArticle( BSTR Title, BSTR Article, /*[out, retval]*/ long *hRes )
+{
+    *hRes = pureParser.AddArticle(Title, Article);
+    return *hRes;
+}
+
+STDMETHODIMP COROSSParserCOM::SaveTitle(BSTR Title, /*[out, retval]*/ long *hRes)
+{
+    *hRes = pureParser.SaveTitle(Title);
+    return *hRes;
+}
+
---
+++ OROSSParser/COROSSParserCOM.h
@@ -0,0 +1,53 @@
+#ifndef __OROSSPARSER_H_
+#define __OROSSPARSER_H_
+
+#include <atlcom.h>
+
+#include "resource.h"       // main symbols
+
+#include "COROSSParser.h"
+
+class ATL_NO_VTABLE COROSSParserCOM : 
+    public ATL::CComObjectRootEx<ATL::CComSingleThreadModel>,
+    public ATL::CComCoClass<COROSSParserCOM, &CLSID_OROSSParser>,
+    public ATL::IDispatchImpl<IOROSSParser, &IID_IOROSSParser, &LIBID_OROSSParserLib>
+{
+
+public:
+    COROSSParserCOM() {};
+
+    DECLARE_REGISTRY_RESOURCEID(IDR_OROSSPARSER)
+
+    DECLARE_PROTECT_FINAL_CONSTRUCT()
+
+    BEGIN_COM_MAP(COROSSParserCOM)
+        COM_INTERFACE_ENTRY(IOROSSParser)
+        COM_INTERFACE_ENTRY(IDispatch)
+    END_COM_MAP()
+public:
+    STDMETHOD(Init)(modeName mode, /*[out, retval]*/ long* hRes);
+    STDMETHOD(Terminate)(/*[out, retval]*/ long* hRes);
+    STDMETHOD(AddPart)( BSTR Name, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddTile)( BSTR Name, /* [out, retval]*/ long *hRes );
+    STDMETHOD(AppendTile)( BSTR Str, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddPara)( long Num, BSTR Para, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddExamplesToPara)( BSTR Examples, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddRule)( BSTR Num, BSTR Rule, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddRuleTitle)(BSTR Num, BSTR Title, /*[out, retval]*/ long *hRes);
+    STDMETHOD(AddOrthogr)( BSTR Orthogr, BSTR Formula, BSTR Example, BSTR Rest, long IsActive, long IsPrefix, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddArticle)( BSTR Title, BSTR Article, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddInfoToRule)( BSTR Info, /*[out, retval]*/ long *hRes );
+    STDMETHOD(AddFootNote)( long ID, BSTR Text, /*[out, retval]*/ long *hRes );
+    //
+//    STDMETHOD(LoadDic)( BSTR Dic, /*[out, retval]*/ long *hRes );
+    STDMETHOD(LoadArticles)( BSTR Dic, /*[out, retval]*/ long *hRes );
+    STDMETHOD(LoadWords)( BSTR Dic, /*[out, retval]*/ long *hRes );
+    // Helpers
+    STDMETHOD(SaveTitle)(BSTR Title, /*[out, retval]*/ long *hRes);
+
+protected:
+
+    COROSSParser pureParser;
+};
+
+#endif //__KHPARSER_H_
