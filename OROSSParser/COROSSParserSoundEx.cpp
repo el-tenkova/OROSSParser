5ac119f087fc711916d5150cf6f1aa572f1d6287
el-tenkova <el-tenkova@yandex.ru>
2017-02-12 08:31:54 +0300

    utf8

--- OROSSParser/COROSSParserSoundEx.cpp
+++ OROSSParser/COROSSParserSoundEx.cpp
@@ -5,19 +5,19 @@
 #include "COROSSParserSoundEx.h"
 COROSSSoundEx::COROSSSoundEx()
 {
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'À', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'à', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'ß', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'ÿ', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'Î', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'î', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'¨', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'¸', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'İ', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'ı', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'Å', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'å', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'È', 0));
-    symMap.insert(std::pair<wchar_t, wchar_t>(L'è', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ°', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ¯', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ñ', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ¾', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ñ‘', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ­', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ñ', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ•', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğµ', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ˜', 0));
+    symMap.insert(std::pair<wchar_t, wchar_t>(L'Ğ¸', 0));
 }
 
