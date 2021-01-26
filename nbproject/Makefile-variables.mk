8f9d917b55f48d2e071b86b08293348f3bae2d67
el-tenkova <el-tenkova@yandex.ru>
2021-01-27 00:07:37 +0300

    NetBeans files and main for linux

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU_5.4-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU_5.4-Linux
+CND_ARTIFACT_NAME_Debug=rebuilder
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU_5.4-Linux/rebuilder
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU_5.4-Linux/package
+CND_PACKAGE_NAME_Debug=rebuilder.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU_5.4-Linux/package/rebuilder.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=rebuilder
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/rebuilder
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=rebuilder.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/rebuilder.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
