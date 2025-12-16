d77ce2c4a118ccc2d1b551bcf8e338ad6310305d
Tenkova Elena <el-tenkova@yandex.ru>
2025-12-16 20:48:08 +0300

    makeABC

--- OROSSParser/COROSSParser.cpp
+++ OROSSParser/COROSSParser.cpp
@@ -274,8 +274,8 @@ long COROSSParser::Terminate()
  //   if (mode == Rebuild)
     {
   //  if (mode != Create && mode != Update) {
-        presaveArticles(SAVE_SEARCH);
-        saveData(SAVE_SEARCH);
+        presaveArticles(LOAD_SEARCH); // SAVE_SEARCH);
+        saveData(LOAD_SEARCH); // SAVE_SEARCH);
         makeSQL();
     }
 //    saveData(SAVE_SEARCH);
