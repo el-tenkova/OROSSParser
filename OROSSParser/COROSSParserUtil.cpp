9f44ec66d6e216a9d5b3328c62ef5cf5bf729245
Tenkova Elena <el-tenkova@yandex.ru>
2025-12-24 17:45:56 +0300

    revert previous commit

--- OROSSParser/COROSSParserUtil.cpp
+++ OROSSParser/COROSSParserUtil.cpp
@@ -727,11 +727,18 @@ std::wstring COROSSParser::toRTF(const std::wstring& article) {
     }
     size_t pos = art.find(L"&#x301;");
     while (pos != std::wstring::npos) {
+        art.insert(pos - 1, L"<f1>");
+        pos = art.find(L"&#x301;");
+        art.replace(pos, wcslen(L"&#x301;"), L"<f0>");
+        pos = art.find(L"&#x301;", pos);
+    }
+/*
+    while (pos != std::wstring::npos) {
         art.insert(pos, L"\u0301");
         pos = art.find(L"&#x301;");
         art.replace(pos, wcslen(L"&#x301;"), L"");
         pos = art.find(L"&#x301;", pos);
-    }
+    } */
     pos = art.find(L'<');
     size_t i = 0;
     while ( i < art.length()) {
