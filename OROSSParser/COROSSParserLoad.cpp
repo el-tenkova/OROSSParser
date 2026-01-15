e9f932cbd9d7895f5be8cbb56f347d04bd883cba
Tenkova Elena <el-tenkova@yandex.ru>
2026-01-15 20:47:36 +0300

    rebuild tutorial (add formulas and orthogr)

--- OROSSParser/COROSSParserLoad.cpp
+++ OROSSParser/COROSSParserLoad.cpp
@@ -39,6 +39,13 @@ void COROSSParser::loadSearchData(bool loadSearch)
                     paras.insert(std::pair<size_t, para>((size_t)Num, cp));
                     curPara = paras.find(Num);
                 }
+                if (mode != Create)
+                {
+                    pararest pr = { parts[2], parts[3] };
+                    curPara->second.links.insert(std::pair<std::wstring, pararest>(parts[1], pr));
+                    restMap::iterator it = curPara->second.links.find(parts[1]);
+                    it->second.min_len = getSearchMinLen(it->second.search);
+                }
             }
         }
         rest.close();
